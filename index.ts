bd2cb7691e5366d0a675889454f78e327eb5b890
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2018-08-01 12:42:51 -0300

    Fix expression + Improve doc

--- index.ts
+++ index.ts
@@ -1,17 +1,17 @@
 /**
  * Take value from object using a string for path expression or return
- * placeholder if value is undefined of unreachable.
+ * placeholder if value is undefined or unreachable.
  * @example ```js
  * take(object, 'data.users[0].contact.phones[0]');
  * take(values, '[3].toString()', '0');
  * ```
  * @param object
- * @param path - Path used get value.
- * @param placeholder - Value returned when can't take property value.
+ * @param path - Path expression used to take value from object.
+ * @param placeholder - Returned when value is undefined or unreachable.
  */
 const take = (object: object, path: string, placeholder: any = null) => {
   try {
-    const expression = 'return o' + path[0] !== '[' ? '.' + path : path;
+    const expression = 'return o' + (path[0] !== '[' ? '.' + path : path);
     const value = new Function('o', expression)(object);
     return value === undefined ? placeholder : value;
   } catch (_) {
