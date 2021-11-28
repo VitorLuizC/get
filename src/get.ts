0f06831b5576d21640c396b6793bc44f1dd5e1f1
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2021-11-28 12:32:10 -0300

    📝 improve JSDocs

--- src/get.ts
+++ src/get.ts
@@ -4,20 +4,39 @@ import type TypeAtPath from './types/TypeAtPath';
 export type { Path, TypeAtPath };
 
 /**
- * Gets value from object using a string for its path expression, if value is
- * 'undefined' or unreachable returns the placeholder instead.
+ * Gets value from object using received path (with dots and positions), if
+ * value is 'undefined' or unreachable returns the placeholder instead.
+ *
+ * @example
+ * get(object, 'data.users[0].contact.phones[0]');
+ * get(values, '[3].count', '0');
+ */
+function get<T, P extends Path<T>, U>(
+  object: T,
+  path: P,
+  placeholder: U,
+): TypeAtPath<T, P> | U;
+
+function get<T, P extends Path<T>>(
+  object: T,
+  path: P,
+): TypeAtPath<T, P> | undefined;
+
+function get(object: unknown, path: string, placeholder?: unknown): unknown;
+
+/**
+ * Gets value from object using received path (with dots and positions), if
+ * value is 'undefined' or unreachable returns the placeholder instead.
  * @example ```js
  * get(object, 'data.users[0].contact.phones[0]');
  * get(values, '[3].count', '0');
  * ```
- * @param object
- * @param path - Path expression used to get value from object.
- * @param [placeholder] - Returned when value is undefined or unreachable.
+ * @param {Object.<string, *>} object
+ * @param {string} path - Path expression used to get value from object.
+ * @param {*} [placeholder] - Returned when value is undefined or unreachable.
+ * @returns {*}
  */
-function get<T, P extends Path<T>, U>(object: T, path: P, placeholder: U): TypeAtPath<T, P> | U;
-function get<T, P extends Path<T>>(object: T, path: P): TypeAtPath<T, P> | undefined;
-function get(object: unknown, path: string, placeholder?: unknown): unknown;
-function get(object: unknown, path: string, placeholder?: unknown): unknown {
+function get(object: any, path: string, placeholder?: any): any {
   try {
     const expression = 'return o' + (path[0] !== '[' ? '.' + path : path);
     const value = new Function('o', expression)(object);
